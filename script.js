abb77256f9001b5d0a91670885583d686c402e8a
mkbuczek <buczekkmason@gmail.com>
2025-04-09 15:25:20 -0400

    v0.1 finish later

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+const grid = [
+    [0, 0, 0, 1, 0],
+    [0, 1, 1, 1, 0],
+    [0, 1, 0, 1, 0],
+    [1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+];
+
+const shapes = [
+    
+]
+
+window.onload = solve;
+
+function solve()
+{
+
+}
